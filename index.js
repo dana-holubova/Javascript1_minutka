f4e38c69cf5cac776800809f98e59d7c88e3c565
dana-holubova <danmich@email.cz>
2023-12-06 17:01:34 +0100

    Zvonění a prompt.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('funguju!');
+
+const ring = () => {
+    document.querySelector(".alarm").classList.add("alarm--ring")
+}
+
+setTimeout(ring, prompt("Napište, za kolik vteřin má budík začít zvonit:") * 1000)
