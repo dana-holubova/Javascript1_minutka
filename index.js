5747b7cda932a0694d50a232cef80e71ed922848
dana-holubova <danmich@email.cz>
2023-12-07 09:56:38 +0100

    Přidání tlačítka na ukončení zvonění.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-console.log('funguju!');
+const cancelRingBtn = document.querySelector("#cancelRingBtn")
 
 const ring = () => {
+    cancelRingBtn.disabled = "disabled"
     document.querySelector(".alarm").classList.add("alarm--ring")
+    document.body.addEventListener("click", () => { document.querySelector("audio").play() })
 }
 
-setTimeout(ring, prompt("Napište, za kolik vteřin má budík začít zvonit:") * 1000)
+const cancelRing = () => {
+    clearTimeout(timerId)
+    cancelRingBtn.textContent = "Zvonění zrušeno"
+    cancelRingBtn.disabled = "disabled"
+}
+
+const time = Number(prompt("Napište, za kolik vteřin má minutka začít zvonit:"))
+
+const timerId = setTimeout(ring, time * 1000)    
+
+cancelRingBtn.addEventListener("click", cancelRing)
